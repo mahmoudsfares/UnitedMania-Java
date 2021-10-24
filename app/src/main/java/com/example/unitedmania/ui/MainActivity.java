443444ca8d44c29b5cf5a3e4b92763f7e20f7fef
msaeed <msaeed@minapharm.com>
2021-10-24 13:26:17 +0200

    Add up button to action bar

--- app/src/main/java/com/example/unitedmania/ui/MainActivity.java
+++ app/src/main/java/com/example/unitedmania/ui/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.unitedmania.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import android.os.Bundle;
+import android.view.MenuItem;
+import com.example.unitedmania.R;
 import com.example.unitedmania.databinding.ActivityMainBinding;
 
 
@@ -12,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+
+        navController.addOnDestinationChangedListener((NavController.OnDestinationChangedListener)
+                (controller, destination, arguments) -> getSupportActionBar().setDisplayHomeAsUpEnabled(destination.getId() != R.id.newsFragment));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            onBackPressed();
+        return super.onOptionsItemSelected(item);
     }
 }
